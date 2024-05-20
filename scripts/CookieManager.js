dad6cefa52af04e18c54e2d21e6c113cc70102c0
TechAle <alessandro.condello.email@gmail.com>
2024-05-20 19:35:26 +0200

    Some improvements

--- scripts/CookieManager.js
+++ scripts/CookieManager.js
@@ -21,11 +21,15 @@ function getCookie(cname) {
     return "";
 }
 
-function delete_cookie( name, path, domain ) {
-    if( get_cookie( name ) ) {
-        document.cookie = name + "=" +
-            ((path) ? ";path="+path:"")+
-            ((domain)?";domain="+domain:"") +
-            ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+function deleteAllCookiesAndRefresh() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/;domain=" + window.location.hostname;
     }
+
+    location.reload(); // Refresh the page
 }
