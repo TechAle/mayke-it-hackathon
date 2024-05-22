55a1866676eeb9d45c2df26d58babf2083e498a5
TechAle <alessandro.condello.email@gmail.com>
2024-05-23 00:39:03 +0200

    Reward to quests

--- scripts/quests/questsArchive.js
+++ scripts/quests/questsArchive.js
@@ -2,31 +2,31 @@ const questStory = {
     "Mani1": {
         "description": "Mani Jewel has been stuck on his phone for hours and hours. She needs your help managing this!",
         "requiredQuest": "",
-        "reward": [""],
+        "reward": ["Knowledge 20"],
         "conversation": "Mani-Jewel-Start"
     },
     "Mani2": {
         "description": "Mani Jewel has been stuck on his phone for hours and hours. Maybe Goldie can help you with this!",
         "requiredQuest": "Mani1",
-        "reward": [""],
+        "reward": ["Awareness 50"],
         "conversation": "Goldie-Mani-Quest"
     },
     "Mani3": {
         "description": "Investigate this TubeYuber that Mani Jewel is so obsessed with!",
         "requiredQuest": "Mani2",
-        "reward": [""],
+        "reward": ["Meditation 25"],
         "conversation": "TubeYuber-Mani-Quest"
     },
     "Mani4": {
         "description": "Report back to Goldie what you discovered",
         "requiredQuest": "Mani3",
-        "reward": [""],
+        "reward": ["Emotional-regulation 25"],
         "conversation": "Goldie-Mani-Quest-2"
     },
     "Mani5": {
         "description": "Give the advices to Mani Jewel",
         "requiredQuest": "Mani4",
-        "reward": [""],
+        "reward": ["Positive-self-image 25"],
         "conversation": "Mani-Jewel-End"
     }
 }
