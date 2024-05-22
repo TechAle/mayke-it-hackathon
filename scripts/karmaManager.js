73412e17a4a270cab40a16c9412bc7d0441cb132
TechAle <alessandro.condello.email@gmail.com>
2024-05-22 18:59:48 +0200

    Added new karma cards

--- scripts/karmaManager.js
+++ scripts/karmaManager.js
@@ -1,13 +1,48 @@
 const karmaCards = {
-    "karma1": {
-        "name": "First karma",
-        "description": "This is the first karma",
-        "icon": "karma1.jpeg",
+    "awareness": {
+        "name": "awareness",
+        "description": "Karma for awareness",
+        "icon": "awareness.png",
     },
-    "karma2": {
-        "name": "Second karma",
-        "description": "This is the second karma",
-        "icon": "karma2.png",
+    "emotionalAwareness": {
+        "name": "emotionalAwareness",
+        "description": "Karma for emotional awareness",
+        "icon": "emotionalAwareness.png",
+    },
+    "emotionalRegulation": {
+        "name": "emotionalRegulation",
+        "description": "Karma for emotional regulation",
+        "icon": "emotionalRegulation.png",
+    },
+    "journaling": {
+        "name": "journaling",
+        "description": "Karma for journaling",
+        "icon": "journaling.png",
+    },
+    "knowledge": {
+        "name": "knowledge",
+        "description": "Karma for knowledge",
+        "icon": "knowledge.png",
+    },
+    "meditation": {
+        "name": "meditation",
+        "description": "Karma for meditation",
+        "icon": "meditation.png",
+    },
+    "physicalAwareness": {
+        "name": "physicalAwareness",
+        "description": "Karma for physical awareness",
+        "icon": "physicalAwareness.png",
+    },
+    "positiveSelfImage": {
+        "name": "positiveSelfImage",
+        "description": "Karma for positive self image",
+        "icon": "positiveSelfImage.png",
+    },
+    "sleepHygiene": {
+        "name": "sleepHygiene",
+        "description": "Karma for sleep hygiene",
+        "icon": "sleepHygiene.png",
     },
 }
 
